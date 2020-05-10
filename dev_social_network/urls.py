baeb6f68a1bb82aea835e9f74d03a33e06a51dda
Adilwebpy <64836330+Adilwebpy@users.noreply.github.com>
2020-05-10 23:17:16 +0600

    Update urls.py

--- dev_social_network/urls.py
+++ dev_social_network/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path(
@@ -32,6 +34,9 @@ urlpatterns = [
         include('profiles.urls', namespace='profiles')
     ),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 
